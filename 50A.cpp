c95b9acb3560e19f3a2c2318301488bb046d14f7
QuangXOR <richardnguyen6231@gmail.com>
2025-06-04 19:29:16 +0700

    Domino piling

---
+++ 50A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << floor(n*m*0.5);
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+
+    return 0;
+}
